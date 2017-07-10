d0ecd8f7b10930ffc2650adb068b9148005082c1
tenie <snoopy32@qq.com>
2017-07-10 15:40:21 +0800

    add pojp

---
+++ src/parent/pojo/src/main/java/net/tenie/pojo/BlogComment.java
@@ -0,0 +1,7 @@
+package net.tenie.pojo;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("blog_comment")
+public class BlogComment    extends Model {}
