fc51ccef7a5195eb3f5103b5bbbd0a868e49f79d
tenie <turenjiemail@foxmail.com>
2017-08-31 18:32:13 +0800

    update

--- src/parent/web/src/main/java/net/tenie/web/service/CecheResult.java
+++ src/parent/web/src/main/java/net/tenie/web/service/CecheResult.java
@@ -1,5 +1,11 @@
 package net.tenie.web.service;
  
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import net.tenie.web.entity.Result;
 
 
@@ -7,6 +13,33 @@ public class CecheResult {
 	
 	private static Result logincacheRS;
 	private static Result cacheRS;
+	
+	private static Calendar calendar;
+	
+	private static ConcurrentHashMap<String, Map> sessionMap = new ConcurrentHashMap<String, Map>();
+	
+	//sessionID 加入到Map
+	public static void addSession(String key,Map value) {
+		//如果已经在里面了
+		if(sessionMap.containsKey(key)) {
+//			Map m = sessionMap.get(key); 
+		}else {
+			sessionMap.put(key, value);
+		}
+		
+		if(calendar !=null ) {
+			Calendar now = Calendar.getInstance();
+			
+		}
+		//删除前一天的session
+		Set<Entry<String, Map>> vals = sessionMap.entrySet();
+		for(Entry entry: vals) { 
+			Map rsM = (Map) entry.getValue();
+			rsM.get("Time");
+			//TODO 如果时间是昨天的就删除;
+		}
+		
+	}
 	
 	//设置为null
 	public static void setNullCacheRS(){
@@ -28,7 +61,13 @@ public class CecheResult {
 	public static void setCacheRS(Result cache) {
 		 cacheRS = cache; 
 	}
-	
+	public static ConcurrentHashMap getSessionMap() {
+		return sessionMap;
+	}
+//	public static void setSessionMap(ConcurrentHashMap sessionMap) {
+//		CecheResult.sessionMap = sessionMap;
+//	}
+//	
 	
 	
 }
